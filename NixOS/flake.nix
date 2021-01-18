200c69110f12ee6c815a543091dcbb458c4856b7
archaict <crosley40@gmail.com>
2021-01-18 10:21:57 +0700

    update flakes

--- NixOS/flake.nix
+++ NixOS/flake.nix
@@ -1,13 +1,24 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/release-20.09";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nur.url = "github:nix-community/NUR";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    nur = {
+      url = "github:nix-community/NUR";
+    };
+
+    unstable = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
   };
+
   outputs = { home-manager, nixpkgs, ... }: {
     nixosConfigurations = {
       hostname = nixpkgs.lib.nixosSystem {
