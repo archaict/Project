b4b77308dbfafc7f4ac50603b8224c54d8ee7461
archaict <crosley40@gmail.com>
2021-01-18 10:04:35 +0700

    update flakes

--- NixOS/flake.nix
+++ NixOS/flake.nix
@@ -3,4 +3,21 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
     home-manager.url = "github:nix-community/home-manager";
   };
+  outputs = { home-manager, nixpkgs, ... }: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home.manager = {
+              useGlobalPkgs = true;
+              users.archaict = ./codex.nix;
+            };
+          }
+        ];
+      };
+    };
+  };
 }
