70df21e7480a4b282abc31613fec7ca239b7b891
archaict <crosley40@gmail.com>
2021-01-18 09:58:53 +0700

    update flakes

--- NixOS/flake.nix
+++ NixOS/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/tree/nixos-20.09";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
     home-manager.url = "github:nix-community/home-manager";
   };
 }
