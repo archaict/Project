9929a4fcd33646f2d328c18919515d06af4c3b83
archaict <crosley40@gmail.com>
2021-02-01 15:40:31 +0700

    revision

--- NixOS/core/corepkgs.nix
+++ NixOS/core/corepkgs.nix
@@ -150,7 +150,17 @@
 
     optpkgs = [
 
-      cataclysm-dda
+    # (cataclysm-dda-git.override {
+    #     version = "2021-02-01";
+    #     rev     = "cebff34972ec27f550d5d891d22949b78144447b"; 
+    #     sha256  = "0fm5swbczyprbb5w0dcd7mbprgwpc5rj5wvxwfj3i46lnk5y99ws";
+    #     useXdgDir = true;
+    #   })
+
+    # cataclysm-dda-git.withMods ( mods: with mods; [
+    #   tileset.UndeadPeople
+    # ])
+
       nethack
 
     ];
--- NixOS/flake.nix
+++ NixOS/flake.nix
@@ -1,5 +1,26 @@
 {
+  description = "An example NixOS configuration";
+
   inputs = {
-    home-manager.url = "github:nix-community/home-manager/";
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-20.09"; };
+    nur = { url = "github:nix-community/NUR"; };
+  };
+
+  outputs = inputs:
+    let ignoreme = ({config,lib,...}: with lib; { system.nixos.revision = mkForce null; system.nixos.versionSuffix = mkForce "pre-git"; }); in
+  {
+    nixosConfigurations = {
+
+      mysystem = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (import ./configuration.nix)
+
+        ];
+        specialArgs = { inherit inputs; };
+      };
+    };
   };
 }
+
+
